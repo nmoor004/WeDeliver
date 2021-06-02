2a6ed9f4df1543b4cc44d50d0a8fb099b4f5cec7
Michelle Dozal <62925991+Michelle-Dozal@users.noreply.github.com>
2021-06-01 17:47:46 -0700

    Changed react image and adder cart image to folder

--- we-deliver/src/App.js
+++ we-deliver/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './WeDeliverLogo.jpg';
 import './App.css';
 
 function App() {
@@ -7,19 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello WeDeliver!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
+  
   );
 }
 
+
 export default App;
+
